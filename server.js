f3fcd3e3f826fad8514f4525621edf9beb419b7c
kristinavd <kristina.vidakovic403@gmail.com>
2025-03-22 22:52:32 +0100

    Environment configuration

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-export const PORT = process.env.PORT;
+export const { PORT, DB_URI } = process.env;
+
+const databaseConnection = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log("Connected to MongoDB...");
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+export default databaseConnection;
