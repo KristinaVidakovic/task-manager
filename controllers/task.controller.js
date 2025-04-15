8bb8a9d876a809bff53987fa8fe67e6b0f026275
kristinavd <kristina.vidakovic403@gmail.com>
2025-04-15 17:46:34 +0200

    Get all tasks

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -16,4 +16,38 @@ const createTask = async (req, res, next) => {
   }
 };
 
-export { createTask };
+const getAllTasks = async (req, res, next) => {
+  try {
+    const status = req.query.status;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const order = req.query.sort === 'asc' ? 1 : -1;
+    const sortField = 'createdAt';
+
+    const query = {
+      userId: req.user._id,
+    };
+
+    if (status) {
+      query.status = status;
+    }
+
+    const allTasks = await Task.find(query).sort({ [sortField]: order })
+      .skip(skip)
+      .limit(limit);
+    const total = await Task.countDocuments();
+
+    return res.status(200).json({
+      tasks: allTasks,
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit)
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createTask, getAllTasks };
